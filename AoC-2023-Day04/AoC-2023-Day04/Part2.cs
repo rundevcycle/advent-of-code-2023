1a450fe0d69b07a8ef67e3ec50dcf9908da71112
Andy Smith <andy@rundevcycle.com>
2023-12-11 22:11:51 -0400

    Day 4, part 2.  Could use some optimization.

--- AoC-2023-Day04/AoC-2023-Day04/Part2.cs
+++ AoC-2023-Day04/AoC-2023-Day04/Part2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace AoC_2023_Day04
     public class Part2
     {
         private string _inputData = "";
+
+        public int FinalSum { get; set; }
 
 
         public Part2(string inputData)
@@ -19,8 +22,45 @@ namespace AoC_2023_Day04
 
         public void Run()
         {
+            var inputLines = _inputData.Replace("\r", "").Split('\n');
 
+            var originalCards = new List<Card>();
+            foreach (var line in inputLines)
+            {
+                var card = new Card(line);
+                originalCards.Add(card);
+            }
+
+            foreach (var card in originalCards)
+            {
+                FinalSum += CardsWon(card, originalCards);
+            }
+
+            Console.WriteLine($"Part 2: Final sum is {FinalSum}.");
         }
 
+
+
+        private int CardsWon(Card card, List<Card> originalCards)
+        {
+            // Recursive works, but it's slow.  There might be another approach to make it faster.
+
+            int count = 1;  // This card, at least.
+            if (card.NumberOfMatches == 0)
+            {
+                return count;
+            }
+            var extraCards = originalCards.Where(c =>
+                c.CardNumber > card.CardNumber
+                && c.CardNumber <= card.CardNumber + card.NumberOfMatches);
+
+            foreach (var extra in extraCards)
+            {
+//                Console.WriteLine($"Extra card: {extra.CardNumber} with {extra.NumberOfMatches} matches.");
+                count += CardsWon(extra, originalCards);
+            }
+
+            return count;
+        }
     }
 }
